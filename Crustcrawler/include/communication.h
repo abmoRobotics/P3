2bd28aad73684d2d36a2e68a0eb872fcf2e789a5
abmoRobotics <56405924+abmoRobotics@users.noreply.github.com>
2020-09-30 14:23:20 +0200

    Nye functions
    d

---
+++ Crustcrawler/include/communication.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+namespace communication {
+    double getTorque() {};
+    double setTorque() {}; // Måske ikke lav
+    double getPosition() {}; // Position in radians
+    double setPosition() {}; // Måske ikke lav
+    double getVelocity() {};
+    double setVelocity() {};
+    double getAcceleration() {};
+    double setAcceleration() {};
+}
+
+
+
